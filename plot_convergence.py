17c8e3b3f895944c26d9f97d42497b9aebd3a048
mjacobse <44684927+mjacobse@users.noreply.github.com>
2019-06-24 17:14:11 +0200

    Add convergence plot in compl space

--- plot_convergence.py
+++ plot_convergence.py
@@ -7,10 +7,12 @@ def plot_results(results, filename):
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
-    matplotlib.rcParams['figure.figsize'] = (16, 9)
 
+    dot_size = 5000000 / (len(results.not_converged_x) +
+                          len(results.converged_x))
+
+    matplotlib.rcParams['figure.figsize'] = (16, 9)
     plt.figure()
-    size = 5000000 / (len(not_converged_x) + len(converged_x))
     plt.scatter(results.not_converged_x, results.not_converged_mult_x,
                 edgecolors='none', marker='.', c='black', s=dot_size)
     plt.scatter(results.converged_x, results.converged_mult_x,
@@ -24,6 +26,22 @@ def plot_results(results, filename):
     plt.xlabel('$x$')
     plt.ylabel('$\\lambda_x$')
     plt.savefig(filename + '.png', format='png')
+    plt.close()
+
+    matplotlib.rcParams['figure.figsize'] = (9, 9)
+    plt.figure()
+    plt.scatter(results.not_converged_x * results.not_converged_mult_x,
+                results.not_converged_s * results.not_converged_mult_s,
+                edgecolors='none', marker='.', c='black', s=dot_size)
+    plt.scatter(results.converged_x * results.converged_mult_x,
+                results.converged_s * results.converged_mult_s,
+                edgecolors='none', marker='.', c=results.converged_iterations,
+                cmap='YlGnBu', s=dot_size)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlim(1e-8, 1e8)
+    plt.ylim(1e-8, 1e8)
+    plt.savefig(filename + '_compl.png', format='png')
     plt.close()
 
 
